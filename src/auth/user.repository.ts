d48f3bf3ee3b258ee59a0764927976f228155f84
narko <narko@narko.com>
2020-06-12 23:37:12 +0300

    signup completed

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,28 @@
 import { User } from "./user.entity";
 import { Repository, EntityRepository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
+  }
 
 }
